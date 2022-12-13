a6e2bed1d19a2c130c92782b9bb2157366a67862
B <77552838+Mrballer59@users.noreply.github.com>
2022-12-13 09:51:06 +0100

    sort fixed + unit test added

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,13 +2,13 @@
  * @jest-environment jsdom
  */
 
-import { fireEvent, screen, waitFor } from "@testing-library/dom";
+import { screen, waitFor, fireEvent } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import Bills from "../containers/Bills.js";
-
+import mockStore from "../__mocks__/store";
 import router from "../app/Router.js";
 // test page employée for the receipt
 describe("Given I am connected as an employee", () => {
@@ -36,29 +36,28 @@ describe("Given I am connected as an employee", () => {
     });
 
     test("Then bills should be ordered from earliest to latest", () => {
-      // the tickets/ bills are suppose to be earliste to oldest
       document.body.innerHTML = BillsUI({ data: bills });
       const dates = screen
         .getAllByText(
           /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
         )
         .map((a) => a.innerHTML);
-      //Change the to sorting.
-      //[Bug report] - Bills
-      const antiChrono = (a, b) => a - b;
+      const antiChrono = (a, b) => (a > b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
   });
 });
+
 //This part is test handleClickIconEye ligne 14 containers/Bills.js
 describe("when a user clicks on the eye icon", () => {
-  test("Then the modal should open", () => {
+  test("Then modal should open", () => {
     // the test the shows the click on the Eye Icon
     Object.defineProperty(window, localStorage, { value: localStorageMock });
     window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
     const html = BillsUI({ data: bills });
     document.body.innerHTML = html;
+
     const onNavigate = (pathname) => {
       document.body.innerHTML = ROUTES({ pathname });
     };
@@ -78,6 +77,47 @@ describe("when a user clicks on the eye icon", () => {
     firstEyeIcon.addEventListener("click", handleClickIconEye);
     fireEvent.click(firstEyeIcon);
     expect(handleClickIconEye).toHaveBeenCalled();
-    expect($.fn.modal).toHaveBeenCalled;
+    expect($.fn.modal).toHaveBeenCalled();
+  });
+});
+
+// test naviagtion ligne 21 containers/Bills.js
+describe("When a user click the button 'Nouvelle note de frais'", () => {
+  test("Then newbill appears", () => {
+    // Path to acces new bill
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+    const billsPage = new Bills({
+      document,
+      onNavigate,
+      store: null,
+      bills: bills,
+      localStorage: window.localStorage,
+    });
+    //test function
+    const OpenNewBill = jest.fn(billsPage.handleClickNewBill);
+    const btnNewBill = screen.getByTestId("btn-new-bill"); //new button
+    btnNewBill.addEventListener("click", OpenNewBill); //event listener
+    fireEvent.click(btnNewBill);
+    // we check if the function is called when expected
+    expect(OpenNewBill).toHaveBeenCalled();
+    expect(screen.getByText("Envoyer une note de frais")).toBeTruthy(); // new note de frais
+  });
+});
+
+// test const bills ligne 37-57 containers/Bills.js
+describe("When I get bills", () => {
+  test("Then it should render bills", async () => {
+    const bills = new Bills({
+      document,
+      onNavigate,
+      store: mockStore,
+      localStorage: window.localStorage,
+    });
+    const getBills = jest.fn(() => bills.getBills());
+    const value = await getBills();
+    expect(getBills).toHaveBeenCalled();
+    expect(value.length).toBe(4);
   });
 });
--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,12 +20,10 @@ const row = (bill) => {
 };
 
 const rows = (data) => {
+  console.log(data);
   return data && data.length
     ? data
-        .sort(function (a, b) {
-          return a.date < b.date ? 1 : -1;
-        })
-
+        .sort((a, b) => (a.date > b.date ? 1 : -1))
         .map((bill) => row(bill))
         .join("")
     : "";
